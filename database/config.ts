5415c5124435a5a4c77db01222c3e183e94a5a43
MariaMaiese <maria.maiese@correoaiep.cl>
2023-11-22 22:14:08 -0300

    cambio de archivos a typescript usuarios y config

---
+++ database/config.ts
@@ -0,0 +1,14 @@
+import { Sequelize } from 'sequelize';
+const PORTDATABASE = process.env.PORTDATABASE
+const HOSTNAME: any = process.env.HOSTNAME
+const USERNAME: any = process.env.USERNAME
+const PASSWORD: any = process.env.PASSWORD
+const DATABASE: any = process.env.DATABASE
+
+
+const sequelize = new Sequelize(DATABASE, USERNAME, PASSWORD, {
+    host: HOSTNAME,
+    dialect: 'mysql'
+});
+
+export default { sequelize };

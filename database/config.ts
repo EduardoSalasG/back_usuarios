446bbb13a7d5041bc4c21b2b18a6cf243de6c663
MariaMaiese <maria.maiese@correoaiep.cl>
2023-11-23 22:25:28 -0300

    arreglando conexion a base de datos

--- database/config.ts
+++ database/config.ts
@@ -1,4 +1,5 @@
 import { Sequelize } from 'sequelize';
+import fs from 'fs';
 
 const PORTDATABASE: any = process.env.PORTDATABASE
 const HOSTNAME: any = process.env.HOSTNAME
@@ -9,7 +10,14 @@ const DATABASE: any = process.env.DATABASE
 
 const sequelize = new Sequelize(DATABASE, USERNAME, PASSWORD, {
     host: HOSTNAME,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectOptions: {
+        ssl: {
+            ca: fs.readFileSync('../DigiCertGlobalRootCA.crt.pem'),
+            rejectUnauthorized: false
+        },
+        port: PORTDATABASE
+    },
 });
 const testConection = async (sequelizeInstance: any) => {
     try {

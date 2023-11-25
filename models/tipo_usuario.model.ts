81f0893df7d5da2e42de585408747156e7ca38eb
EduardoSalasG <eduardo.salasgo@correoaiep.cl>
2023-11-25 16:50:07 -0300

    Actualización de modelos

--- models/tipo_usuario.model.ts
+++ models/tipo_usuario.model.ts
@@ -1,5 +1,7 @@
 import { DataTypes, Model } from "sequelize";
 import { sequelize } from "../database/config";
+import { usuario } from "./usuario.model";
+import { tipo_usuario_usuario } from "./tipo_usuario_usuario.model";
 
 
 export class tipo_usuario extends Model {}
@@ -20,3 +22,6 @@ tipo_usuario.init({
   sequelize, //We need to pass the connection instance
   modelName: 'tipo_usuario'
 });
+
+tipo_usuario.belongsToMany(usuario, { as: 'USU_ID_usuario_tipo_usuario_usuarios', through: tipo_usuario_usuario, foreignKey: "TUS_ID", otherKey: "USU_ID" });
+tipo_usuario.hasMany(tipo_usuario_usuario, { as: "tipo_usuario_usuarios", foreignKey: "TUS_ID"});
